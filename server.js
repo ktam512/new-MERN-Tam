10b26e599a90e4b46e27f4699edbf8e0c2dd73f1
ktam512 <ktam512@gmail.com>
2024-07-19 16:53:29 +0700

    Add changes to server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieparser = require("cookie-parser");
-// hehe
+const fileUpload= require('express-fileupload')
+const cookieParser = require("cookie-parser");
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -11,7 +12,36 @@ app.use(cors())
 app.use(fileUpload({
   useTempFiles:true
 }))
-
 // connect to mongodb
-//Hello world second time
-//Hellow world
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(URI, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+.then((res) => {
+  console.log('Connected to DB')
+})
+.catch((error) => {
+  console.log(error);
+});
+
+
+app.get('/', (req,res)=>{
+  res.json({msg:'Welcome to the e-commerce world!'})
+})
+
+const PORT = process.env.PORT || 3000
+
+const server = app.listen(PORT , ()=>{
+  console.log('server is up and running', PORT)
+})
+
+// Handle server errors
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${PORT} is already in use. Please use a different port.`);
+  } else {
+    console.error('An error occurred:', err);
+  }
+});
