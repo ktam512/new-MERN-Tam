fc3df295ae52d23b6b13bf97509f84edd2b16c57
ktam512 <ktam512@gmail.com>
2024-07-19 20:48:29 +0700

    Add changes to userCtrl and userRouter

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,7 +1,26 @@
 const Users = require('../models/userModel')
+const bcrypt = require ('bcrypt')
 const userCtrl = {
-register:(req,res)=>{
-    res.json({msg: "test controller"})
+register:async(req,res)=>{
+    try {
+        const {name, email, password} = req.body
+
+        const user = await Users.findOne({email})
+        if(user) return res.status(400).json({msg:"this email already exists"})
+
+        if(password.length < 6) 
+        return res.status(400).json({msg:'password should be atleast 6 characters longs'})
+
+        const passwordHash = await bcrypt.hash(password, 10)
+        const newUser = new Users({
+            name, email, password:passwordHash
+        })
+        await newUser.save();
+        
+        return res.json({msg:"register success"})
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
 }
 }
 module.exports = userCtrl
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
 const userCtrl = require('../controllers/userCtrl')
 
-router.post('/register',(req,res)=>{
-    res.json({msg: "Test Router"})
-})
+router.post('/register',userCtrl.register)
 
 module.exports = router
