ead6750b4767d9a9a3b7227f9c3b0970fc5f39ba
ktam512 <ktam512@gmail.com>
2024-07-22 09:54:52 +0700

    Add key code (define 'file') in upload.js

--- routers/upload.js
+++ routers/upload.js
@@ -17,19 +17,19 @@ router.post('/upload', (req,res)=>{
         if(!req.files || Object.keys(req.files).length === 0)
             return res.status(400).json({msg: 'No files were uploaded'})
 
+        const file = req.files.file;
+
         if(file.size > 1024*1024) {  
-            removeTemp(file.tempFilePath)
             return res.status(400).json({msg: 'Size is too large'})
         }
         if( file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-            removeTemp(file.tempFilePath)
             return res.status(400).json({msg: 'file format is incorrect'})
         }
 
         cloudinary.v2.uploader.upload(file.tempFilePath, {folder:"test"},async(err,result)=>{
             if(err) throw err;
-            removeTemp(file.tempFilePath)
-            res.json({public_id: result.public_id, url: result.secure_url})
+            
+            res.json({result})
         })
     
     } catch (err) {
