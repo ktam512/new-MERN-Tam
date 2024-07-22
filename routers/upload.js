702beae9396bed7d5d3b6dd83ab8a18166dd9e4a
ktam512 <ktam512@gmail.com>
2024-07-22 20:33:16 +0700

    Add destroy image and remove unneeded path
    Add destroy image function and remove temporary path

--- routers/upload.js
+++ routers/upload.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cloudinary = require('cloudinary');
+const fs = require ('fs')
 
 const auth = require('../middlewares/auth');
 const authAdmin = require('../middlewares/authAdmin')
@@ -11,23 +12,25 @@ cloudinary.config({
 
 })
 
-router.post('/upload', (req,res)=>{
+router.post('/upload',auth, authAdmin, (req,res)=>{
     try {
         if(!req.files || Object.keys(req.files).length === 0)
             return res.status(400).json({msg: 'No files were uploaded'})
 
         const file = req.files.file;
 
-        if(file.size > 1024*1024) {  
+        if(file.size > 1024*1024) {
+            removeTemp(file.tempFilePath)  
             return res.status(400).json({msg: 'Size is too large'})
         }
         if( file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+            removeTemp(file.tempFilePath)
             return res.status(400).json({msg: 'file format is incorrect'})
         }
 
         cloudinary.v2.uploader.upload(file.tempFilePath, {folder:"test"},async(err,result)=>{
             if(err) throw err;
-            
+            removeTemp(file.tempFilePath)
             res.json({public_id: result.public_id, url: result.secure_url})
         })
     
@@ -35,5 +38,26 @@ router.post('/upload', (req,res)=>{
         res.status(500).json({msg: err.message})
      }
  })
+
+ router.post('/destroy', auth, authAdmin, (req,res)=>{
+    try {
+       const {public_id} = req.body; 
+       if(!public_id) return res.status(400).json({msg:'No image selected'})
+
+       cloudinary.v2.uploader.destroy(public_id, async(err, result)=>{
+           if(err) throw err;
+
+           res.json({msg: 'Deleted image successfully'})
+       })
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+})
+
+ const removeTemp = (path) =>{
+    fs.unlink(path,err =>{
+        if(err) throw err;
+    })
+}
 
 module.exports= router;
