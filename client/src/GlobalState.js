e31af820f57c3c2b23bf7ab595b4422f9eebdf01
ktam512 <ktam512@gmail.com>
2024-07-23 22:21:40 +0700

    Add ProductAPI and connect to db

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,9 +1,11 @@
 import React, {createContext, useState, useEffect} from 'react'
+import ProductsAPI from "./api/ProductsAPI"
 
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
+    ProductsAPI()
     return (
         <GlobalState.Provider value={"value in Global"}>
             {children}
--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
-import Products from "./products/Products"
-import Login from "./auth/Login"
-import Register from "./auth/Register"
-import Cart from "./cart/Cart"
-import NotFound from "./utils/not_found/NotFound"
+import { Routes, Route } from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/not_found/NotFound'
 
 function Pages() {
     return (
         <Routes>
-             <Route path="/" exact component={Products}/>
-             <Route path="/login" exact component={Login}/>
-             <Route path="/register" exact component={Register}/>
-             <Route path="/cart" exact component={Cart}/>
-
-             <Route path="*" exact component={NotFound}/>
+            <Route path="/" element={<Products />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
