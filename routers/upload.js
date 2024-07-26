ee57e84776b19e40b95eedca2d3608bff3b5a000
ktam512 <ktam512@gmail.com>
2024-07-26 15:14:03 +0700

    Make changes in upload.js

--- routers/upload.js
+++ routers/upload.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const cloudinary = require('cloudinary');
 const fs = require ('fs')
-
 const auth = require('../middlewares/auth');
 const authAdmin = require('../middlewares/authAdmin')
 
@@ -12,34 +11,37 @@ cloudinary.config({
 
 })
 
-router.post('/upload',auth, authAdmin, (req,res)=>{
+router.post('/upload', auth, authAdmin, (req,res) =>{
     try {
+      
         if(!req.files || Object.keys(req.files).length === 0)
-            return res.status(400).json({msg: 'No files were uploaded'})
+        return res.status(400).json({msg: 'No files were uploaded'})
 
-        const file = req.files.file;
+        const file= req.files.file;
 
-        if(file.size > 1024*1024) {
-            removeTemp(file.tempFilePath)  
+        if(file.size > 1024*1024) {  
+            removeTemp(file.tempFilePath)
             return res.status(400).json({msg: 'Size is too large'})
         }
         if( file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
             removeTemp(file.tempFilePath)
             return res.status(400).json({msg: 'file format is incorrect'})
         }
+       
 
         cloudinary.v2.uploader.upload(file.tempFilePath, {folder:"test"},async(err,result)=>{
             if(err) throw err;
             removeTemp(file.tempFilePath)
             res.json({public_id: result.public_id, url: result.secure_url})
         })
-    
-    } catch (err) {
-        res.status(500).json({msg: err.message})
-     }
- })
 
- router.post('/destroy', auth, authAdmin, (req,res)=>{
+        
+    } catch (err) {
+       return res.status(500).json({msg: err.message})
+    }
+})
+
+router.post('/destroy', auth, authAdmin, (req,res)=>{
     try {
        const {public_id} = req.body; 
        if(!public_id) return res.status(400).json({msg:'No image selected'})
@@ -54,7 +56,7 @@ router.post('/upload',auth, authAdmin, (req,res)=>{
     }
 })
 
- const removeTemp = (path) =>{
+const removeTemp = (path) =>{
     fs.unlink(path,err =>{
         if(err) throw err;
     })
