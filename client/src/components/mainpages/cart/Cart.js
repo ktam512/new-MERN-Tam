3bc22c71d8df19def8d929f00fb6e9b5163c929f
ktam512 <ktam512@gmail.com>
2024-07-26 21:54:53 +0700

    Add the payment and counting section in Cart

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -5,28 +5,36 @@ import {Link} from "react-router-dom"
 function Cart() {
     const state = useContext(GlobalState)
     const [cart] = state.userAPI.cart
-    if(cart.length === 0) 
-        return <h2 style={{textAlign:'center' , fontSize:"5rem"}}>Cart Empty</h2>
+    if(cart.length === 0) return <h2 style={{textAlign:'center' , fontSize:"5rem"}}>Cart Empty</h2>
     return (
         <div>
         {
-            cart.map(product =>{
-                <div className="detail">
-            <img src={product.images.url} alt=""/>
-            <div className="box-detail">
-                <div className="row">
-                    <h2>{product.title}</h2>
-                    <h6>ID: {product.product_id}</h6>
-                </div>
-                <h5>${product.price}</h5>
-                <p> <h4 style={{textDecoration:'underline' , color:'whitesmoke'}}>Content:</h4> {product.content}</p>
-                <p><h4 style={{textDecoration:'underline' , color:'whitesmoke'}}>Description : </h4>{product.description}</p>
-                <p>SOLD:  {product.sold}</p>
-                <Link to="/cart" className="cart">BUY NOW</Link>
+            cart.map(product => (
+                <div className="detail cart">
+                <img src={product.images.url} alt="" className="img_container"/>
+                <div className="box-detail">
+                    
+                <h2>{product.title}</h2>
+                       
+                    
+                <h5>${product.price * product.quantity}</h5>
+                    <p> <h4 style={{textDecoration:'underline' , color:'whitesmoke'}}>Content : </h4>{product.content}</p>
+                    <p> <h4 style={{textDecoration:'underline' , color:'whitesmoke'}}>Description : </h4>{product.description}</p>
+                    <div className="amount">
+                        <button > - </button>
+                        <span>{product.quantity}</span>
+                        <button> + </button>
+                    </div>
+                 <div className="delete"> x </div>
+                
             </div>
-        </div>
-            })
+            </div>
+            ))
         }
+        <div className="total">
+            <h3>Total : $ 0 </h3>
+            <Link to="#"> Payment</Link>
+        </div>
         </div>
     )
 }
